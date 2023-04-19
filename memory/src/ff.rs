70e5c8d83a769c0f2ebbb39abfb7307053bb4720
xlang <xlangk@proton.me>
2023-04-18 21:59:33 -0400

    Added module tests

--- memory/src/ff.rs
+++ memory/src/ff.rs
@@ -1,43 +1,114 @@
 //! FF (Flip Flop) module contains logical FF emulators which perform realistic operations
 //! of which would be done by a real FF integrated circuit.
 
-/// SRFF (Set Reset Flip Flop) is a FF (Flip Flop) with 2 inputs.
+/// SRFF (Set Reset Flip Flop) is a clocked FF (Flip Flop) with 2 inputs.
 /// This FF allows for basic bit or word setting with reset
 /// functionality.
 pub struct SrFf {
-	bit: bool,
+	pub bit: bool,
+	pub clock: bool,
+
+	/// If this is true, the flip flop will trigger on the falling edge of the clock,
+	/// otherwise it will trigger on the rising edge.
+	pub trigger_falling: bool,
+
+	/// This is the input of the flip flip that is queued to be applied when the clock
+	/// meets the trigger mode.
+	pub input_queue: bool,
+
+	/// Reset input queue which is used to keep track of the alternate input.
+	pub reset_queue: bool,
 }
 
 impl SrFf {
-	pub fn new() -> Self {
+	/// # Arguments
+	///  - trigger_mode: When true, the flip flop will trigger on the falling edge of the clock,
+	/// otherwise it will trigger on the rising edge.
+	pub fn new(trigger_falling: bool) -> Self {
 		Self {
 			bit: false,
+			clock: false,
+			trigger_falling,
+			input_queue: false,
+			reset_queue: false,
 		}
 	}
 
-	pub fn set(&mut self) {
-		self.bit = true;
+	pub fn set(&mut self, input_bit: bool) {
+		self.input_queue = input_bit;
 	}
 
-	pub fn reset(&mut self) {
-		self.bit = false;
+	pub fn reset(&mut self, input_bit: bool) {
+		self.reset_queue = input_bit;
 	}
-}
 
-impl Default for SrFf {
-	fn default() -> Self {
-		Self::new()
+	pub fn set_clock(&mut self, clock_bit: bool) {
+		if self.clock != clock_bit {
+			self.clock = clock_bit;
+			if (self.trigger_falling && !self.clock) || (!self.trigger_falling && self.clock) {
+				if self.reset_queue && !self.input_queue {
+					self.bit = false;
+				} else if !self.reset_queue && self.input_queue {
+					self.bit = true;
+				} else if self.reset_queue && self.input_queue {
+					self.bit = !self.bit;
+				}
+			}
+		}
 	}
 }
 
 #[test]
-fn test_sr_ff() {
-	let mut sr_ff = SrFf::new();
+fn test_sr_ff_rising() {
+	let mut sr_ff = SrFf::new(false);
 
-	sr_ff.set();
+	// Test basic set control
+	sr_ff.set(true);
+	sr_ff.set_clock(false);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(true);
 	assert!(sr_ff.bit);
 
-	sr_ff.reset();
+	// Test toggle logic
+	sr_ff.reset(true);
+	sr_ff.set_clock(false);
+	assert!(sr_ff.bit);
+	sr_ff.set_clock(true);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(false);
+
+	// Test reset control
+	sr_ff.set(false);
+	sr_ff.set_clock(true);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(false);
+	assert!(!sr_ff.bit);
+}
+
+#[test]
+fn test_sr_ff_falling() {
+	let mut sr_ff = SrFf::new(true);
+
+	// Test basic set control
+	sr_ff.set(true);
+	sr_ff.set_clock(true);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(false);
+	assert!(sr_ff.bit);
+
+	// Test toggle logic
+	sr_ff.reset(true);
+	sr_ff.set_clock(true);
+	assert!(sr_ff.bit);
+	sr_ff.set_clock(false);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(true);
+
+	// Test reset control
+	sr_ff.set(false);
+	sr_ff.set_clock(false);
+	assert!(!sr_ff.bit);
+	sr_ff.set_clock(true);
 	assert!(!sr_ff.bit);
 }
 
@@ -65,7 +136,7 @@ impl TFf {
 		Self {
 			bit: false,
 			clock: false,
-			trigger_falling: false,
+			trigger_falling,
 			input_queue: false,
 		}
 	}
@@ -77,7 +148,8 @@ impl TFf {
 	pub fn set_clock(&mut self, clock_bit: bool) {
 		if self.clock != clock_bit {
 			self.clock = clock_bit;
-			if (self.trigger_falling && !self.clock) || (!self.trigger_falling && self.clock) { self.bit = self.input_queue; }
+			if ((self.trigger_falling && !self.clock) || (!self.trigger_falling && self.clock))
+				&& self.input_queue { self.bit = !self.bit; }
 		}
 	}
 }
@@ -86,46 +158,70 @@ impl TFf {
 fn test_t_ff_rising() {
 	let mut t_ff = TFf::new(false);
 
+	// Test basic toggle from false
+	assert!(!t_ff.bit);
 	t_ff.set_input(true);
 	t_ff.set_clock(false);
 	assert!(!t_ff.bit);
-
 	t_ff.set_clock(true);
 	assert!(t_ff.bit);
-
 	t_ff.set_clock(false);
-	assert!(t_ff.bit);
 
+	// Test basic toggle from true
+	assert!(t_ff.bit);
+	t_ff.set_clock(true);
+	assert!(!t_ff.bit);
+	t_ff.set_clock(false);
+
+	// Ensure false input does not change state
 	t_ff.set_input(false);
-	assert!(t_ff.bit);
-	t_ff.set_clock(true);
 	assert!(!t_ff.bit);
 	t_ff.set_clock(true);
 	assert!(!t_ff.bit);
 	t_ff.set_clock(false);
+
+	// Ensure dual clock duplicate state does not change state
+	t_ff.set_input(true);
 	assert!(!t_ff.bit);
+	t_ff.set_clock(true);
+	assert!(t_ff.bit);
+	t_ff.set_clock(true);
+	assert!(t_ff.bit);
+	t_ff.set_clock(false);
 }
 
 #[test]
 fn test_t_ff_falling() {
 	let mut t_ff = TFf::new(true);
 
+	// Test basic toggle from false
+	assert!(!t_ff.bit);
 	t_ff.set_input(true);
 	t_ff.set_clock(true);
 	assert!(!t_ff.bit);
-
 	t_ff.set_clock(false);
 	assert!(t_ff.bit);
-
 	t_ff.set_clock(true);
-	assert!(t_ff.bit);
 
+	// Test basic toggle from true
+	assert!(t_ff.bit);
+	t_ff.set_clock(false);
+	assert!(!t_ff.bit);
+	t_ff.set_clock(true);
+
+	// Ensure false input does not change state
 	t_ff.set_input(false);
-	assert!(t_ff.bit);
-	t_ff.set_clock(false);
 	assert!(!t_ff.bit);
 	t_ff.set_clock(false);
 	assert!(!t_ff.bit);
 	t_ff.set_clock(true);
+
+	// Ensure dual clock duplicate state does not change state
+	t_ff.set_input(true);
 	assert!(!t_ff.bit);
+	t_ff.set_clock(false);
+	assert!(t_ff.bit);
+	t_ff.set_clock(false);
+	assert!(t_ff.bit);
+	t_ff.set_clock(true);
 }
